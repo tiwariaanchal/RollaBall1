67a13ed22cc196468cb5ddf088f9b61e3304160c
tiwariaanchal <49729337+tiwariaanchal@users.noreply.github.com>
2020-03-09 10:28:57 -0500

    Initial commit

---
+++ Assets/3DGamekit/Scripts/Game/DamageSystem/DeathVolume.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Gamekit3D
+{
+    [RequireComponent(typeof(Collider))]
+    public class DeathVolume : MonoBehaviour
+    {
+        //public new AudioSource audio;
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            SceneManager.LoadScene(0);
+           /* var pc = other.GetComponent<PlayerController>();
+            if (pc != null)
+            {
+                pc.Die(new Damageable.DamageMessage());
+            }
+            if (audio != null)
+            {
+                audio.transform.position = other.transform.position;
+                if (!audio.isPlaying)
+                    audio.Play();
+            }*/
+        }
+
+        void Reset()
+        {
+          /*  if (LayerMask.LayerToName(gameObject.layer) == "Default")
+                gameObject.layer = LayerMask.NameToLayer("Environment");
+            var c = GetComponent<Collider>();
+            if (c != null)
+                c.isTrigger = true;*/
+        }
+
+    }
+}
---
+++ Assets/Bouncer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    [SerializeField]
+    float bouncePower = 100;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        PlayerController pr = collision.gameObject.GetComponent<PlayerController>();
+        Vector3 bounce = Vector3.Reflect(pr.lastVelocity, collision.contacts[0].normal);
+
+        rb.AddForce(bounce.normalized * bouncePower);
+    }
+}
